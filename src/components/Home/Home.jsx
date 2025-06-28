ed93bff5034c12f0717759f0fa1ca9fb449f4403
Olivia Chattopadhyay <olivia@Olivias-MacBook-Air.local>
2025-06-28 11:36:55 +0530

    Edited Home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -15,8 +15,9 @@ export default function Home() {
         </h1>
         <h2 className="text-2xl lg:text-4xl mt-4 text-gray-300 tracking-wide">
           3rd Year B.Tech Computer Science Student
-        </h2>
+       </h2>
 
+        
         <button
           onClick={() => navigate('/contact')}
           className="mt-8 px-6 py-3 rounded-md bg-blue-600 hover:bg-blue-700 transition-colors text-white font-semibold"
@@ -26,7 +27,7 @@ export default function Home() {
       </div>
 
       {/* Right: Vertical line + tagline */}
-      <div className="flex items-center  ">
+      {/* <div className="flex items-center justify-center pl-5">
         <div className="hidden lg:flex h-50 border-l-2 border-gray-500 mr-6 "></div>
        
         <p className="hidden md:flex text-xl md:text-2xl text-yellow-300 mt-6 font-mono">
@@ -41,7 +42,7 @@ export default function Home() {
         />
 </p>
 
-      </div>
+      </div> */}
     </div>
   );
 }
